6b72f2638837e3e4ef5be072fa3a8054924f966f
Alfonso Villalobos <alfonsocvu@gmail.com>
2023-08-23 14:10:33 -0600

    Adding window management remaps

--- lua/chato/remap.lua
+++ lua/chato/remap.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<C-t>", vim.cmd.vsp)
+vim.keymap.set("n", "<C-x>", vim.cmd.q)
+vim.keymap.set("n", "<C-s>", vim.cmd.w)
+vim.keymap.set("n", "<C-q>", vim.cmd.qa)
 
 vim.opt.number = true
