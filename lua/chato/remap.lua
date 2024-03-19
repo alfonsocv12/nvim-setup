90b78e07de39a10c90095ee36f20bf13130318fe
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-03-19 10:29:39 -0600

    Sync linux with remap clipboard

--- lua/chato/remap.lua
+++ lua/chato/remap.lua
@@ -5,5 +5,7 @@ vim.keymap.set("n", "<C-t>", vim.cmd.vsp)
 vim.keymap.set("n", "<C-x>", vim.cmd.q)
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
 vim.keymap.set("n", "<C-q>", vim.cmd.qa)
+vim.o.clipboard = "unnamedplus"
+vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true, silent = true })
 
 vim.opt.number = true
