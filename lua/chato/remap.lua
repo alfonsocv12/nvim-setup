8280fdf0e1c0f6348c744624295dece074f6f2ea
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-03-19 10:26:22 -0600

    Update linux

--- lua/chato/remap.lua
+++ lua/chato/remap.lua
@@ -5,6 +5,8 @@ vim.keymap.set("n", "<A-t>", vim.cmd.vsp)
 vim.keymap.set("n", "<C-x>", vim.cmd.q)
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
 vim.keymap.set("n", "<A-q>", vim.cmd.qa)
+vim.o.clipboard = "unnamedplus"
 vim.api.nvim_set_keymap("n", '<A-w>', '<C-w>', { noremap = true })
+vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true})
 
 vim.opt.number = true
