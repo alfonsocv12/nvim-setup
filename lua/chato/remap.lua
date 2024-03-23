1f851249df16bb2f099fcc80ff92061c7a45480f
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-03-23 16:46:49 -0600

    Remap jump to definition

--- lua/chato/remap.lua
+++ lua/chato/remap.lua
@@ -7,6 +7,8 @@ vim.keymap.set("n", "<D-x>", vim.cmd.q)
 vim.keymap.set("n", "<D-s>", vim.cmd.w)
 vim.keymap.set("n", "<D-q>", vim.cmd.qa)
 vim.o.clipboard = "unnamedplus"
+
+vim.api.nvim_set_keymap("n", "<D-]>", "<C-]>", { noremap = true })
 vim.api.nvim_set_keymap("n", '<A-w>', '<C-w>', { noremap = true })
 vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true})
 
