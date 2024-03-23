540f04762c37165e197603ffeebcd2080c8af7ad
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-03-22 22:02:05 -0600

    change to command

--- lua/chato/remap.lua
+++ lua/chato/remap.lua
@@ -1,10 +1,11 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<C-\\>", vim.cmd.NvimTreeToggle)
-vim.keymap.set("n", "<A-t>", vim.cmd.vsp)
-vim.keymap.set("n", "<C-x>", vim.cmd.q)
-vim.keymap.set("n", "<C-s>", vim.cmd.w)
-vim.keymap.set("n", "<A-q>", vim.cmd.qa)
+-- vim.keymap.set("n", "<A-t>", vim.cmd.vsp)
+vim.keymap.set("n", "<D-t>", vim.cmd.vsp)
+vim.keymap.set("n", "<D-x>", vim.cmd.q)
+vim.keymap.set("n", "<D-s>", vim.cmd.w)
+vim.keymap.set("n", "<D-q>", vim.cmd.qa)
 vim.o.clipboard = "unnamedplus"
 vim.api.nvim_set_keymap("n", '<A-w>', '<C-w>', { noremap = true })
 vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true})
