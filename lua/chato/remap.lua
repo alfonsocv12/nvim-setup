91a082077dad8e566ca6d2606465f005bf42640f
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-03-27 23:48:48 -0600

    Adding os detection

--- lua/chato/remap.lua
+++ lua/chato/remap.lua
@@ -1,9 +1,15 @@
+local output, _ = os.execute("uname")
+
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeToggle)
-vim.keymap.set("n", "<C-t>", vim.cmd.vsp)
-vim.keymap.set("n", "<C-x>", vim.cmd.q)
-vim.keymap.set("n", "<C-s>", vim.cmd.w)
-vim.keymap.set("n", "<C-q>", vim.cmd.qa)
+if output == "Darwin" then
+    vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeToggle)
+    vim.keymap.set("n", "<C-t>", vim.cmd.vsp)
+    vim.keymap.set("n", "<C-x>", vim.cmd.q)
+    vim.keymap.set("n", "<C-s>", vim.cmd.w)
+    vim.keymap.set("n", "<C-q>", vim.cmd.qa)
+end
+
+-- Copy to clipboard
 vim.o.clipboard = "unnamedplus"
 vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true, silent = true })
