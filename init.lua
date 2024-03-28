b6f328b000c822d2ae9d468aaf99047c2c912d1d
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-03-28 00:19:50 -0600

    merge linxu with darwin setup

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+OS_NAME, _ = os.execute("uname")
+OS_CTRL_KEY = OS_NAME == "Linux" and "D" or "C"
+
 require("chato.remap")
 require("chato.packer")
 require("chato.custom-sets")
--- lua/chato/remap.lua
+++ lua/chato/remap.lua
@@ -1,20 +1,15 @@
-local output, _ = os.execute("uname")
-
 vim.g.mapleader = " "
 
-if output == "Darwin" then
+vim.keymap.set("n", string.format("<%s-t>", OS_CTRL_KEY), vim.cmd.vsp)
+vim.keymap.set("n", string.format("<%s-x>", OS_CTRL_KEY), vim.cmd.q)
+vim.keymap.set("n", string.format("<%s-s>", OS_CTRL_KEY), vim.cmd.w)
+vim.keymap.set("n", string.format("<%s-q>", OS_CTRL_KEY), vim.cmd.qa)
+
+if OS_NAME == "Darwin" then
     vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeToggle)
-    vim.keymap.set("n", "<C-t>", vim.cmd.vsp)
-    vim.keymap.set("n", "<C-x>", vim.cmd.q)
-    vim.keymap.set("n", "<C-s>", vim.cmd.w)
-    vim.keymap.set("n", "<C-q>", vim.cmd.qa)
-elseif output == "Linux" then
+elseif OS_NAME == "Linux" then
     vim.keymap.set("n", "<C-\\>", vim.cmd.NvimTreeToggle)
     -- vim.keymap.set("n", "<A-t>", vim.cmd.vsp)
-    vim.keymap.set("n", "<D-t>", vim.cmd.vsp)
-    vim.keymap.set("n", "<D-x>", vim.cmd.q)
-    vim.keymap.set("n", "<D-s>", vim.cmd.w)
-    vim.keymap.set("n", "<D-q>", vim.cmd.qa)
 
     vim.api.nvim_set_keymap("n", "<D-]>", "<C-]>", { noremap = true })
     vim.api.nvim_set_keymap("n", '<D-w>', '<C-w>', { noremap = true })
