05b4a774fcabd2de3f1ed2f5922e30fd5be49467
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-03-27 23:56:27 -0600

    feat: adding linux remaps

--- lua/chato/remap.lua
+++ lua/chato/remap.lua
@@ -8,6 +8,18 @@ if output == "Darwin" then
     vim.keymap.set("n", "<C-x>", vim.cmd.q)
     vim.keymap.set("n", "<C-s>", vim.cmd.w)
     vim.keymap.set("n", "<C-q>", vim.cmd.qa)
+elseif output == "Linux" then
+    vim.keymap.set("n", "<C-\\>", vim.cmd.NvimTreeToggle)
+    -- vim.keymap.set("n", "<A-t>", vim.cmd.vsp)
+    vim.keymap.set("n", "<D-t>", vim.cmd.vsp)
+    vim.keymap.set("n", "<D-x>", vim.cmd.q)
+    vim.keymap.set("n", "<D-s>", vim.cmd.w)
+    vim.keymap.set("n", "<D-q>", vim.cmd.qa)
+
+    vim.api.nvim_set_keymap("n", "<D-]>", "<C-]>", { noremap = true })
+    vim.api.nvim_set_keymap("n", '<D-w>', '<C-w>', { noremap = true })
+    vim.api.nvim_set_keymap("n", "<D-6>", "<C-^>", { noremap = true })
+    vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true})
 end
 
 -- Copy to clipboard
