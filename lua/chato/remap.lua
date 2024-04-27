df06d36b39efb5942ce683d1cd37678c96b99309
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-04-27 13:04:29 -0600

    feat: start trying to fix

--- lua/chato/remap.lua
+++ lua/chato/remap.lua
@@ -5,6 +5,13 @@ vim.keymap.set("n", string.format("<%s-t>", OS_CTRL_KEY), vim.cmd.vsp)
 vim.keymap.set("n", string.format("<%s-x>", OS_CTRL_KEY), vim.cmd.q)
 vim.keymap.set("n", string.format("<%s-s>", OS_CTRL_KEY), vim.cmd.w)
 vim.keymap.set("n", string.format("<%s-q>", OS_CTRL_KEY), vim.cmd.qa)
+vim.keymap.set("n", string.format("<%s-\\>", OS_CTRL_KEY), vim.cmd.NvimTreeToggle)
+
+if OS_NAME == "Linux" then
+    vim.api.nvim_set_keymap("n", '<D-w>', '<C-w>', { noremap = true })
+    vim.api.nvim_set_keymap("n", '<D-t>', '<C-t>', { noremap = true })
+end
+
 
 -- Move lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -28,16 +35,11 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Find and replace selacted word
 vim.keymap.set("n", "<leader>sr", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/g<left><left><left><left>")
 
-if OS_NAME == "Darwin" then
-    vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeToggle)
-elseif OS_NAME == "Linux" then
-    vim.keymap.set("n", "<C-\\>", vim.cmd.NvimTreeToggle)
-    -- vim.keymap.set("n", "<A-t>", vim.cmd.vsp)
-
+-- Navigation
+if OS_NAME == "Linux" then
     vim.api.nvim_set_keymap("n", "<D-]>", "<C-]>", { noremap = true })
-    vim.api.nvim_set_keymap("n", '<D-w>', '<C-w>', { noremap = true })
+    vim.api.nvim_set_keymap("n", "<D-o>", "<C-o>", { noremap = true })
     vim.api.nvim_set_keymap("n", "<D-6>", "<C-^>", { noremap = true })
-    vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true})
 end
 
 -- Copy to clipboard
